1f2d2541efa5511b9a6ee7af50c2f3bf0e2038df
vpatel9202 <vpatel9202@gmail.com>
2020-04-09 21:03:39 -0400

    filing_data_from_org_json

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from requests.exceptions import HTTPError
 import json
+import pandas
 
 
 # ---------------------------------------------------------------------------- #
@@ -17,9 +18,7 @@ pp_search_url = 'https://projects.propublica.org/nonprofits/api/v2/search.json'
 pp_org_url = 'https://projects.propublica.org/nonprofits/api/v2/organizations/{}.json'
 
 
-# ---------------------------------------------------------------------------- #
-#                               Request Functions                              #
-# ---------------------------------------------------------------------------- #
+# ----------------------------- API Interactions ----------------------------- #
 
 # Test for an HTTP response and if not "200", throw error.
 def get_http_response(url):
@@ -51,5 +50,17 @@ def save_json_to_file(write_data, write_file):
 def read_json_from_file(read_file):
     with open(read_file) as f:
         read_data = json.load(f)
-    
+
     return read_data
+
+
+# ----------------------------- Data Manipulation ---------------------------- #
+
+# Read complete organization JSON file and return filing data as pandas dataframe
+def filing_data_from_org_json(filename):
+    json_data = read_json_from_file(filename)
+    filing_data = json_data['filings_with_data']
+    columns = filing_data[0].keys()
+
+    df = pandas.DataFrame(filing_data, columns=columns)
+    return df
